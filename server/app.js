d2cc9e14332236d3f035d5edf7be21dfc9a5db15
Luiza-Georgiana Alexandru <radu91luiza@gmail.com>
2024-02-20 14:02:53 +0200

    Added server folder

---
+++ server/app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+  // Add headers
+  app.use((req, res, next) => {
+
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT);
+
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+
+      // Pass to next layer of middleware
+      next();
+  });
+
+app.get('/', (req, res) => {
+  res.send('index.html')
+})
+
+app.post('/', (req,res) => {
+  let data = req.body;
+  res.send('Data Received: ' + JSON.stringify(data));
+  console.log(req)
+
+})
+
+module.exports = app;
+
